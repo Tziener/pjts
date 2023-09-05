9fa371a079d99438966ac395d76611e5f0d6e573
Tziener <144073039+Tziener@users.noreply.github.com>
2023-09-04 21:04:14 -0300

    Add files via upload

--- pjt.js
+++ pjt.js
@@ -1,2 +1,15 @@
+const cep1 = document.querySelector("#send")
+
+cep1.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const name1 = document.querySelector("#cep1")
+
+    const value1 = name1.value
+
+    console.log (value1)
+
+});
 
 
